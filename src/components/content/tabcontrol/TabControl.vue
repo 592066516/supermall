e1f3b4e4ef9e9ee6b61848d6b3ab1ad5ec0d5ffc
lvjianjian <592066516@qq.com>
2020-06-04 17:16:43 +0800

    tabcontaol的封装

---
+++ src/components/content/tabcontrol/TabControl.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(item,index) in titles"
+      :key="item"
+      class="tab-control-item"
+      :class="{active:index === currentIndex}"
+      @click="tabClick(index)"
+    >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Tabcontrol",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    };
+  },
+  methods:{
+      tabClick(index) {
+          this.currentIndex = index
+      }
+  }
+ 
+};
+</script>
+<style>
+.tab-control {
+  display: flex;
+}
+.tab-control-item {
+  flex: 1;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+  font-size: 15px;
+  background-color: #fff;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+.active {
+  color: var(--color-high-text);
+}
+.active span {
+  border-bottom: 2px solid red;
+}
+</style>
