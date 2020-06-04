024c76c7d424611ad2ff0d14e426a2ae02637512
lvjianjian <592066516@qq.com>
2020-06-04 10:32:58 +0800

    首页轮播图封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,26 +3,31 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+   <home-swiper :urlImg = "urlImg" />
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childcomps/HomeSwiper"
+
 import { getHomeMultidata } from "network/home";
+
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper
   },
   data() {
     return {
-      urlImg:[]
+      urlImg:[],
     }
   },
   created() {
     getHomeMultidata().then(res => {
       console.log(res);
-      this.urlImg = res.data.banner
+      this.urlImg = res.data.banner.list
     });
   }
 };
