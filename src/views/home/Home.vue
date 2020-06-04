3581422f92bb876cfa76c2842bd9b89da09df158
lvjianjian <592066516@qq.com>
2020-06-04 09:46:58 +0800

    网络模块封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"> 
+    <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-  export default {
-    name: "Home",
-    components:{
-      NavBar
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+export default {
+  name: "Home",
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      urlImg:[]
     }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      console.log(res);
+      this.urlImg = res.data.banner
+    });
   }
+};
 </script>
 
 <style scoped>
-.home-nav{
+.home-nav {
   background-color: var(--color-tint);
   color: #fff;
 }
